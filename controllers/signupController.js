3312d26b0aa570703f5fdb2fa85ee1dd8ac27067
Raviteja1819 <163375780+raviteja-1819@users.noreply.github.com>
2024-05-22 18:19:37 +0530

    signup

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -12,7 +12,14 @@ const signupController = {
     async signup(req, res) {
         try {
           const { firstName, lastName, mobileNumber, email, organization, password } = req.body;
-          console.log(req.body);
+          console.log({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            mobileNumber: req.body.mobileNumber,
+            email: req.body.email,
+            organization: req.body.organization,
+          });
+          
           
           // Check if file upload succeeded
           if (!req.file) {
@@ -41,6 +48,66 @@ const signupController = {
           console.error('Error in signup :', error);
           return res.status(500).json({ error: 'Internal Server Error' });
         }
-      }
+      },
+
+    // get all the users
+async getAllUsers(req, res) {
+    try {
+        const allUsers = await users.findAll(); // Renamed the variable to allUsers
+        return res.status(200).json(allUsers);
+    } catch (error) {
+        console.error('Error in getAllUsers:', error);
+        return res.status(500).json({ error: 'Internal Server Error' });
     }
+}
+,
+// get single user by userId
+async getUserbyuserId(req, res) {
+    try {
+      const { userId } = req.params;
+      const user = await users.findAll({ where: { userId } });
+      return res.status(200).json(user);
+    } catch (error) {
+      console.error('Error in fetching user :', error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  },
+
+  // update user details
+  async updateUser(req, res) {
+    try {
+        const { userId } = req.params;
+        if(!userId){
+            return res.status(404).json({message:"userId required"});
+        }
+        const { firstName, lastName, mobileNumber, email, organization } = req.body;
+        const user = await users.update({ firstName, lastName, mobileNumber, email, organization }, { where: { userId } });
+        return res.status(200).json({ message: 'updated successfully' });
+        } catch (error) {
+            console.error('Error in fetching user :', error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+},
+
+// delete user by userId
+async deleteUser(req, res) {
+    try {
+        const { userId } = req.params;
+        if (!userId) {
+            return res.status(400).json({ error: 'userId is required.' });
+          }
+            const deletedCount = await users.destroy({ where: { userId: userId } });
+            if (deletedCount === 0) {
+            return res.status(404).json({ error: 'user not found.' });
+          }
+          // Return success response
+          return res.status(200).json({ message: 'user deleted successfully.' });
+        } catch (error) {
+          console.error('Error deleting user:', error);
+          return res.status(500).json({ error: 'Internal Server Error' });
+        }
+      }
+    }  
+    
+ 
     module.exports = signupController;
--- routes/signupRoutes.js
+++ routes/signupRoutes.js
@@ -9,13 +9,15 @@ const upload = multer();
 // Route to create 
 router.post('/signup',upload.single('photo'), signupController.signup);
 
-// // Route to get all users
-// router.get('/users', signupController.getAllusers);
+// Route to get all users
+router.get('/users', signupController.getAllUsers);
 
-// // Route to update 
-// router.put('/user/:userId', signupController.updateUser);
+// Route to get single user
+router.get('/users/:userId', signupController.getUserbyuserId);
+// Route to update 
+router.put('/user/:userId', signupController.updateUser);
 
-// // Route to delete 
-// router.delete('/user/:userId', signupController.deleteUser);
+// Route to delete 
+router.delete('/user/:userId', signupController.deleteUser);
 
 module.exports = router;
